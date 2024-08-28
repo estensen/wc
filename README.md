c2f27b62b7ea6681956555a334d9d16fe4b1a4b4
Håvard Anda Estensen <haavard.ae@gmail.com>
2024-08-28 20:04:25 +0200

    Add char and line counting

--- README.md
+++ README.md
@@ -2,7 +2,15 @@
 
 ## Usage
 ```
-echo "hello world" | cargo run
-...
-2
+// Run with stdin
+➜  wc git:(main) ✗ echo "hello world" | cargo run
+    Finished `dev` profile [unoptimized + debuginfo] target(s) in 0.00s
+     Running `target/debug/wc`
+       1        2       12
+
+// Use file as input
+➜  wc git:(main) ✗ cat src/main.rs | cargo run
+    Finished `dev` profile [unoptimized + debuginfo] target(s) in 0.00s
+     Running `target/debug/wc`
+      48      123     1178
 ```
--- src/main.rs
+++ src/main.rs
@@ -5,12 +5,24 @@ fn main() {
     io::stdin()
         .read_to_string(&mut buffer)
         .expect("Failed to read from stdin");
+
+    let line_count = count_lines(&buffer);
     let word_count = count_words(&buffer);
-    println!("{}", word_count);
+    let char_count = count_chars(&buffer);
+
+    println!("{:>8} {:>8} {:>8}", line_count, word_count, char_count);
+}
+
+fn count_lines(input: &str) -> usize {
+    input.lines().count()
 }
 
 fn count_words(input: &str) -> usize {
     input.split_whitespace().count()
+}
+
+fn count_chars(input: &str) -> usize {
+    input.chars().count()
 }
 
 #[cfg(test)]
@@ -18,7 +30,24 @@ mod tests {
     use super::*;
 
     #[test]
-    fn test_empty_words() {
+    fn test_count_lines() {
+        let test_cases = [
+            ("empty string", "", 0),
+            ("single word", "hello", 1),
+            ("multiple words", "hello word", 1),
+            ("much whitespace", "hello     world", 1),
+            ("newlines", "hello\nworld", 2),
+            ("tabs", "hello\tworld", 1),
+        ];
+
+        for (name, input, expected) in &test_cases {
+            let result = count_lines(input);
+            assert_eq!(result, *expected, "Failed on test case: {}", name);
+        }
+    }
+
+    #[test]
+    fn test_count_words() {
         let test_cases = [
             ("empty string", "", 0),
             ("single word", "hello", 1),
@@ -31,6 +60,22 @@ mod tests {
         for (name, input, expected) in &test_cases {
             let result = count_words(input);
             assert_eq!(result, *expected, "Failed on test case: {}", name);
+        }
+    }
+
+    #[test]
+    fn test_char_count() {
+        let test_cases = [
+            ("empty string", "", 0),
+            ("single char", "h", 1),
+            ("multiple words", "hello word", 10),
+            ("newlines", "hello\nworld", 11),
+            ("tabs", "hello\tworld", 11),
+        ];
+
+        for (name, input, expected) in &test_cases {
+            let result = count_chars(input);
+            assert_eq!(result, *expected, "Failed on test case: {}", name)
         }
     }
 }
